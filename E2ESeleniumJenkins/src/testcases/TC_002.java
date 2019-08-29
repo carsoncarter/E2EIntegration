5471746f0498798aa70561e0f41a8397e3779219
Carson A Carter <Carson.Carter@ibm.com>
2019-08-29 09:17:30 -0500

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_002.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("world");
+		driver.quit();
+	}
+}
